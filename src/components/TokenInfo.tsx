3a2ff2bb6bc8bc4da31f55cfe64927e8178e4a94
HYK <junhyeok001010@gmail.com>
2024-11-13 11:20:06 +0900

    1st-3

--- src/components/TokenInfo.tsx
+++ src/components/TokenInfo.tsx
@@ -1,7 +1,9 @@
+import { client } from "@/app/client";
 import { CONTRACT } from "@/app/constants";
 import { formatNumber } from "@/lib/utils";
 import { getContractMetadata } from "thirdweb/extensions/common";
 import { totalSupply } from "thirdweb/extensions/erc20";
+import { MediaRenderer } from "thirdweb/react";
 
 export default async function TokenInfo() {
   const contractMetadata = await getContractMetadata({ contract: CONTRACT });
@@ -9,11 +11,16 @@ export default async function TokenInfo() {
 
   return (
     <div className="mt-4 text-gray-800 p-4 rounded-md bg-slate-300">
+      <MediaRenderer
+        client={client}
+        src={contractMetadata.image}
+        width="150"
+        height="150"
+      />
       <h1 className="text-2xl font-bold mb-2">Token Info</h1>
       <p>Name: {contractMetadata.name}</p>
       <p>Symbol: {contractMetadata.symbol}</p>
       <p>Description: {contractMetadata.description}</p>
-      <p>Total Supply: {formatNumber(total)}</p>
     </div>
   );
 }
